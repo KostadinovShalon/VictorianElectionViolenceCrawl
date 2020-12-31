6ce3ba68d43d0f85e5fe208ccdf49d0017ff8cb7
Brian Isaac <brian.k.isaac-medina@durham.ac.uk>
2020-12-31 04:29:56 +0000

    Clean-up

--- application/__init__.py
+++ application/__init__.py
@@ -21,8 +21,7 @@ def create_app():
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        SECRET_KEY='dev'
     )
     # ensure the instance folder exists
     try:
