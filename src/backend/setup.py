6ce8e603788b632f42e048a3c7742d7e8b017527
KostadinovShalon <kostadinov.shalon@gmail.com>
2021-01-04 18:43:27 +0000

    Changing to 2.1.0

--- src/backend/setup.py
+++ src/backend/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='vev_crawler',
-    version='2.0.5',
+    version='2.1.0',
     python_requires='>=3',
     author='Brian K Isaac Medina',
     url='https://github.com/KostadinovShalon/VictorianElectionViolenceCrawl',
